b96bda38881e6cb15906c01fc51c96b8abe1ddd1
paul k <paul.kervegan@hotmail.fr>
2022-01-09 00:37:58 +0100

    09.01.22 - added routes.py

---
+++ duchamp_reader/routes.py
@@ -0,0 +1,111 @@
+from flask import render_template, url_for, request, flash, redirect
+
+from .app import app
+from .modeles.data_classes import *
+
+@app.route("/")
+def accueil():
+    #...
+
+@app.route("/artiste")
+@app.route("/artiste/<int:id>")
+@app.route("/artiste/add", methods=["GET", "POST"])
+def artiste_ajout():
+    # si l'utilisateur.ice n'est pas connecté.e
+    if current_user.is_authenticated is False:
+        flash("Veuillez vous connecter pour rajouter des données", "error") #syntaxe de flash: flash("message", "category"): "flash("To contribute, log in or sign in", "info")"
+        return redirect("/login")
+    # si il.elle est connecté.e et si un formulaire est envoyé avec POST
+    if request.method == "POST":
+        succes, donnees = Artiste.artiste_new(
+            nom=request.form.get("nom", None),
+            prenom=request.form.get("prenom", None),
+            annee_naissance=request.form.get("annee_naissance", None),
+            genre=request.form.get("genre", None),
+            ville_naissance=request.form.get("ville_naissance", None),
+            ville_residence=request.form.get("ville_residence", None)
+        )
+        if succes is True:
+            flash("Vous avez ajouté un.e nouvel.le artiste à la base de données", "success")
+            return redirect("/artiste") # url de redirection, on peut changer
+        else:
+            flash("Error: " + " + ".join(donnees), "error") # data c'est quoi ?? je l'ai trouvé dans le devoir StoneAdvisor
+            return render_template("pages/artiste_ajout.html") # artiste_ajout : fichier html contenant le formulaire d'ajout d'artiste
+    else:
+        return render_template("pages/artiste_ajout.html")
+
+@app.route("/nomination")
+@app.route("/nomination/<int:id>")
+@app.route("/nomination.add", methods=["GET", "POST"])
+def nomination_ajout():
+    # si l'utilisateur.ice n'est pas connecté.e
+    if current_user.is_authenticated is False:
+        flash("Veuillez vous connecter pour rajouter des données", "error")
+        return redirect("/login")
+    # si il.elle est connecté.e et si un formulaire est envoyé avec post
+    if request.method == "POST":
+        succes, donnees = Nomination.nomination_new(
+            annee=request.form.get("annee", None),
+            laureat=request.form.get("laureat", None),
+            nom_artiste=request.form.get("nom_artiste", None),
+            prenom_artiste=request.form.get("prenom_artiste", None),
+            theme=request.form.get("theme", None)
+        )
+        if succes is True:
+            flash("Vous avez rajouté une nouvelle nomination au prix Marcel Duchamp dans la base", "success")
+            return redirect("/nomination")
+        else:
+            flash("L'ajout de données n'a pas pu être fait: " + " + ".join(donnees), "error")
+            return render_template("pages/nomination_ajout.html")
+    else:
+        return render_template("pages/nomination_ajout.html")
+
+@app.route("/galerie")
+@app.route("/galerie/<int:id>")
+@app.route("/galerie/add")
+def galerie_ajout():
+    # si l'utilisateur.ice n'est pas connecté.e
+    if current_user.is_authenticated is False:
+        flash("Veuillez vous connecter pour rajouter des données", "error")
+        return redirect("/login")
+    # si il.elle est connecté.e et si un formulaire est envoyé avec post
+    if request.method == "POST":
+        succes, donnees = Galerie.galerie_new(
+            nom=request.form.get("nom", None)
+        )
+        if succes is True:
+            flash("Vous avez rajouté une nouvelle galerie à la base de données", "success")
+            return redirect("/artiste")
+        else:
+            flash("L'ajout de données n'a pas pu être fait: " + " + ".join(donnees), "error")
+            return render_template("pages/galerie_ajout.html")
+    else:
+        return render_template("pages/galerie_ajout.html")
+
+@app.route("/ville")
+@app.route("/ville/<int:id>")
+@app.route("/ville/add", methods=["POST", "GET"])
+def ville_ajout():
+    # si l'utilisateur.ice n'est pas connecté.e
+    if current_user.is_authenticated is False:
+        flash("Veuillez vous connecter pour rajouter des données", "error")
+        return redirect("/login")
+    # si il.elle est connecté.e et si un formulaire est envoyé avec post
+    if request.method == "POST":
+        succes, donnees = Ville.ville_new(
+            nom=request.form.get("nom", None),
+            longitude=request.form.get("longitude", None)
+        )
+        if succes is True:
+            flash("Vous avez rajouté une nouvelle ville à la base de données", "success")
+            return redirect("/ville")
+        else:
+            flash("L'ajout de données n'a pas pu être fait: " + " + ".join(donnees), "error")
+            return render_template("pages/ville_ajout.html")
+    else:
+        return render_template("pages/ville_ajout.html")
+
+@app.route("/theme")
+@app.route("/theme/<int:id>")
+@app.route("/theme/add", methods=["POST", "GET"])
+# j'ai pas encore créé ma staticmethod de création pour Theme, donc j'attends pour faire la route
